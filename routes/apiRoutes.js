047ecbe495842518b82d512695c72b74e1376949
Robert Tilton <robtilton2086@gmail.com>
2021-04-26 18:20:52 -0400

    reworking of routes to finally get delete button working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,65 @@
-// import items needed
+const path = require("path");
+const fs = require("fs");
 const router = require('express').Router();
-const db = require('../db/db.json');
-const fs = require('fs');
+let notes = require("../db/db.json")
 
-// make a GET request with all notes from the database
+router.route('/notes')
+    .get((req, res) => {
+        res.json(notes)
+    })
+    .post((req, res) => {
+        const newNote = req.body;
 
-router.get('/notes', (req, res) => {
-    res.json(db);
-})
+        let id = 1;
 
-// create a post request
-router.post('/notes', (req, res) => {
-    // store
-    req.body.id = Math.floor(Math.random() * Math.floor(1000));
-    db.push(req.body);
-    fs.writeFileSync('./db/db.json', JSON.stringify(db));
-})
-// create a delete request
-router.delete('/notes/:id', (req, res) => {
-    // store
-    console.log(req.params.id);
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) throw err;
-        let dbData = JSON.parse(data);
-        console.log(dbData);
-        for (let i = 0; i < dbData.length; i++) {
-            if (dbData[i].id == req.params.id) {
-                console.log("found note");
-                dbData.splice(i, 1);
-                console.log(dbData);
-            }
+        if (notes.length === 0) {
+            newNote.id = 1
+        } else {
+            newNote.id = id + 1
         }
-        fs.writeFileSync('./db/db.json', JSON.stringify(dbData));
-    });
-    res.json(db);
-})
+
+        notes.push(newNote);
+        let jsonNotes = JSON.stringify(notes)
+        fs.writeFile("./db/db.json", jsonNotes, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success!");
+        })
+        res.json(true)
+    })
+
+router.route('/notes/:id')
+    .get((req, res) => {
+        const id = req.params.id;
+        let find;
+        notes.forEach(n => {
+            if (id == n.id) {
+                find = n;
+                return res.json(n)
+            }
+        })
+        res.json(false)
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        notes.forEach((n, index) => {
+            if (id == n.id) {
+                notes.splice(index, 1)
+                const notesCopy = notes.slice();
+                let jsonNotes = JSON.stringify(notesCopy)
+                fs.writeFile("./db/db.json", jsonNotes, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("Successfully deleted!");
+                })
+
+            }
+        })
+        res.json(true);
+    })
 
 
 module.exports = router;
